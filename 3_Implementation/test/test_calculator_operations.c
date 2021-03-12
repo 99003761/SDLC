c769fad332b92d03efd03a1de89c1142a83f4423
99003761 <78851073+99003761@users.noreply.github.com>
2021-03-12 20:53:42 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -6,11 +6,7 @@
 #define PROJECT_NAME    "Calculator"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+
 void test_temp_C_F(void);
 void test_temp_F_C(void);
 void test_Len_M_F(void);
@@ -30,11 +26,7 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+ 
   ////////99003761/////
   RUN_TEST(test_temp_C_F);
   RUN_TEST(test_temp_F_C);
@@ -52,37 +44,7 @@ int main()
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-}
 
-
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
-}
 ///////////////////////////////99003761 Test_Case//////////////////////////////////////////////////////////
 void test_temp_C_F(void)
 {
